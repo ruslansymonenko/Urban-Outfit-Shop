934c61eb463f64ecca94c1690681ee4cd4138d04
Ruslan Symonenko <symonenkors@gmail.com>
2023-03-15 12:48:59 +0200

    installed react, tailwind, and scss

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.parent}>
+      <h1 className={styles.title}>Hello this is online store</h1>
+    </div>
+  );
+}
+
+export default App;
